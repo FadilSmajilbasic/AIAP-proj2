60e521a29133d41cb36d2ef04154c48abef9e0e5
Fadil Smajilbasic <smajilbaicfadil@gmail.com>
2022-05-30 01:15:07 +0200

    made example work with webcam

--- face_detection/face_detection.py
+++ face_detection/face_detection.py
@@ -3,15 +3,21 @@ import cv2
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read the input image
-img = cv2.imread('test.jpg')
-# Convert into grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# Detect faces
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-# Draw rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-# Display the output
-imS = cv2.resize(img, (960, 540))
-cv2.imshow('img', imS)
-cv2.waitKey(0)
+capture = cv2.VideoCapture(0)
+
+if not (capture.isOpened()):
+    while(True):
+        returnCode, frame = capture.read()
+        # Convert into grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Detect faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        # # Draw rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+else:
+    print('Could not open video device')
